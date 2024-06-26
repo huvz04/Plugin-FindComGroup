2931c02235ff1c4df601ce5d1e40236064e01813
huvz <dachen04@outlook.com>
2024-06-26 17:30:42 +0800

    feat(all): update

--- src/preload.js
+++ src/preload.js
@@ -1,8 +1,13 @@
 // Electron 主进程 与 渲染进程 交互的桥梁
 const { contextBridge } = require("electron");
 
-
 // 在window对象下导出只读对象
-contextBridge.exposeInMainWorld("plugin_template", {
-
+contextBridge.exposeInMainWorld("plugin_find_com_group", {
+  updateStyle: (callback) =>
+    ipcRenderer.on(
+      "LiteLoaderQQNT.plugin_find_com_group.updateStyle",
+      callback
+    ),
+  rendererReady: () =>
+    ipcRenderer.send("LiteLoaderQQNT.plugin_find_com_group.rendererReady"),
 });
